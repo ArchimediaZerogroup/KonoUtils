d87090a7e596fadcc47236f0d9e16d1d31a63e43
Marino <marinobonetti@gmail.com>
2017-08-04 10:22:55 +0200

    correzione per scrittura errata tempo per DB

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -4,11 +4,15 @@
 # Where 6N is the number of places after the decimal (.)
 # For less precision (eg. miliseconds), change 6N to 3N
 if defined?(ActiveRecord)
-  if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter
-    version = Gem::Version.new(Mysql2::Client.info.fetch(:version))
-    min_vresion = Gem::Version.new('5.6.4')
-    if version>=min_vresion
-      Time::DATE_FORMATS[:db] = '%Y-%m-%d %H:%M:%S.%6N'
+  rails_version = Rails.gem_version
+  max_version = Gem::Version.new('4.2.5')
+  if rails_version<max_version
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter
+      version = Gem::Version.new(Mysql2::Client.info.fetch(:version))
+      min_vresion = Gem::Version.new('5.6.4')
+      if version>=min_vresion
+        Time::DATE_FORMATS[:db] = '%Y-%m-%d %H:%M:%S.%6N'
+      end
     end
   end
 end
