52e9ff9e9d8d98e2ad8effdc67226e5e7eb7ce6c
Marino Bonetti <marinobonetti@gmail.com>
2017-10-11 15:46:56 +0200

    Correzione in mancanza di Rails

--- lib/kono_utils/encoder.rb
+++ lib/kono_utils/encoder.rb
@@ -3,10 +3,7 @@ module KonoUtils
   # Classe che si occupa di decodificare una qualsiasi stringa in formato utf8,
   # cercando di trovare l'encoding iniziale a tentativi.
   class Encoder
-
-
     attr_accessor :string
-
 
     ##
     # * *Attributes*  :
@@ -15,41 +12,36 @@ module KonoUtils
       self.string = string
     end
 
-
     ##
     # Funcione di rimozione del carattere BOM http://en.wikipedia.org/wiki/Byte_order_mark
     # e encoding normale
     def remove_bom
-      string_encoder.gsub("\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+      string_encoder.gsub("\xEF\xBB\xBF".force_encoding('UTF-8'), '')
     end
 
     ##
     # Funzione di encoding semplice
     def string_encoder
-      return self.string if self.string.valid_encoding?
-      str = self.string
+      return string if string.valid_encoding?
+      str = string
       Encoding.list.each do |e|
         begin
           str.force_encoding(e.name)
-          tmp_string = str.encode("UTF-8")
+          tmp_string = str.encode('UTF-8')
           return tmp_string if tmp_string.valid_encoding?
         rescue
-          logger.debug { "Rescue -> #{e.name}" }
+          Rails.logger.debug { "Rescue -> #{e.name}" } if defined?(::Rails)
         end
       end
 
       impossible_encoding
 
-      self.string
+      string
     end
 
     ##
     # Metodo placeholder, volendo si può estendere la funzione e sovrascrivere questa funzione
     # per essere notificati in caso di mancata decodifica
-    def impossible_encoding
-
-    end
-
-
+    def impossible_encoding; end
   end
 end
