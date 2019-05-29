48bf7db598aefdcc1376b4f533bc6ade8079d109
Marino Bonetti <marinobonetti@gmail.com>
2019-05-29 15:06:10 +0200

    modifica per controllare se utilizzato adapter senza collegarsi

--- config/initializers/mysql.rb
+++ config/initializers/mysql.rb
@@ -4,10 +4,10 @@
 if defined?(ActiveRecord)
   module ActiveRecord::ConnectionAdapters
     if defined?(Mysql2Adapter)
-      if ActiveRecord::Base.connection.instance_of? Mysql2Adapter
+      if ActiveRecord::Base.connection_config[:adapter].downcase == Mysql2Adapter::ADAPTER_NAME.downcase
         version = Gem::Version.new(Mysql2::Client.info.fetch(:version))
         min_vresion = Gem::Version.new('5.6.4')
-        if version>=min_vresion
+        if version >= min_vresion
           AbstractMysqlAdapter::NATIVE_DATABASE_TYPES[:datetime][:limit] = 6
         end
       end
