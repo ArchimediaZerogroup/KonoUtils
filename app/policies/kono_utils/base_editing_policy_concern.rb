646b54719957a2509eaa73cd5fa314c00a651555
Marino Bonetti <marinobonetti@gmail.com>
2019-07-24 09:16:20 +0200

    implementazione logica generica per diversificare le policy campi editabili,visualizzabili,modificabili

--- app/policies/kono_utils/base_editing_policy_concern.rb
+++ app/policies/kono_utils/base_editing_policy_concern.rb
@@ -5,10 +5,32 @@ module KonoUtils
     extend ActiveSupport::Concern
 
     included do
+
+      ##
+      # elenco degli attributi filtrati alla ricezione nel controller
       def permitted_attributes
         # [:descrizione]
-        record.class.column_names.collect { |s| s.to_sym } - [:id, :created_at, :updated_at]
+        cleard_columns
       end
+
+      ##
+      # elenco attributi editabili nella form, che come standard è un alias del permitted
+      def editable_attributes
+        cleard_columns
+      end
+
+      ##
+      # Elenco attributi da visualizzare
+      def show_attributes
+        cleard_columns
+      end
+
+      private
+      def cleard_columns
+        record.class.column_names.collect {|s| s.to_sym} - [:id, :created_at, :updated_at]
+      end
+
+
     end
 
     if defined? ::Application::Scope
